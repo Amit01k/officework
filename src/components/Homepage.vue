10a53034e243308df530ecda9ffca0de83ca789a
Amit Kumar10 <amit.kumar10@olacabs.com>
2022-07-19 15:14:54 +0530

    added more buttons

--- src/components/Homepage.vue
+++ src/components/Homepage.vue
@@ -1,7 +1,7 @@
 <template>
 <!-- <h2>this is the first component that is created by you  {{"amit".length}}   {{10+20}}</h2>     {{10+30}} this is interpolation -->
 <div>
-    <h3>e-mail: {{"iurhtiuhdiutuid"}}</h3>
+    <h3 v-on:mousemove="warn()">e-mail:</h3>
 <h2>gameName function {{getName()}} : this is come from methods getName     {{getData("this is come from getData function")}}   this is third function {{getMailAndName()}}</h2>
 
 
@@ -9,7 +9,7 @@
 
 <button v-on:click="test2('button 2 clicked')"> button 2 click me</button>
 
-<!-- <button ></button> -->
+<button v-on:dblclick="double()">bouble click fucntion</button>
 </div>
 <!-- <h2>this is come from getData :>  {{getData("this is come from getData function")}}</h2> -->
 </template>
@@ -49,6 +49,12 @@ export default{
         test2(data){
             alert(data)
 
+        },
+        double(){
+            alert("this is come from double click fucntion")
+        },
+        warn(){
+            console.warn("this function call when mouse hover")
         }
     }
    
@@ -63,6 +69,9 @@ export default{
 h2{
     color: red;
     background-color: green;
+}
+button{
+    margin: 4px;
 }
 
 
--- src/components/amit.vue
+++ src/components/amit.vue
@@ -2,7 +2,11 @@
 <div>
     <p><b>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi amet repellendus quae vitae, nam modi accusamus! Commodi delectus officia dolores!</b></p>
 <p>ggvhgvjnhvjhjijeriotjierjtioer</p>
+<!-- <h3>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo, laboriosam.</h3>
+<h2>dfgjdbfjgbjdfgbjfd</h2> -->
 </div>
+
+
 
 
 </template>
