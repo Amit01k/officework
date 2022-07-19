52914479aa1b47a752b54092782c37990559527a
Amit Kumar10 <amit.kumar10@olacabs.com>
2022-07-19 16:46:13 +0530

    binding and two way-binding

--- src/components/amit.vue
+++ src/components/amit.vue
@@ -4,6 +4,17 @@
 <p>ggvhgvjnhvjhjijeriotjierjtioer</p>
 <!-- <h3>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo, laboriosam.</h3>
 <h2>dfgjdbfjgbjdfgbjfd</h2> -->
+<!-- <input type="text" v-model="count"  />
+<h2>{{count}}</h2> -->
+
+
+<input type="text" v-model="name"/>
+<br>
+<br>
+<input type="text" v-model="email"/>
+<br>
+<br>
+<button v-on:click="getData()" type="button">button</button>
 </div>
 
 
@@ -13,7 +24,23 @@
 
 <script>
 export default{
-    name:"amit-lorem"
+    name:"amit-lorem",
+    data(){
+        return{
+            email:null,
+            name:null
+        }
+        // return{
+        //     count:100
+        // }
+
+    },
+    methods:{
+        getData(){
+            console.warn("value:",this.email,this.name)
+        }
+
+    }
 }
 
 
