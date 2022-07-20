5dee9d844d13486403c79c45e3a46a7a71c74bad
Amit Kumar10 <amit.kumar10@olacabs.com>
2022-07-20 12:37:08 +0530

    if-else condition working properly

--- src/components/ifelse.vue
+++ src/components/ifelse.vue
@@ -1,8 +1,14 @@
 <template>
 <div>
     <h3 v-if="show">this is come from if condition</h3>
+    <h3 v-else>this is come from else</h3>
+    <button v-on:click="show=!show">toogle    </button>
+    <!-- <button v-on:click="show=false">toogle2    </button> --> <!--if we wan to run this button so we need comment the else consditon-->
 
 <h3 v-if="show2">this disable because here i gived show2(false) value</h3>
+    
+
+<h3 v-else>this disable because here i gived show2(false) value</h3>
 </div>
 
 </template>
@@ -13,8 +19,8 @@ export default{
     data(){
         return{
             show:true,
-            show2:false
-        }
+            // show2:false
+         }
     }
 }
 
