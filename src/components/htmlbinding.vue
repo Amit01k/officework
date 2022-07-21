93ff68afc706a7e4ad01df11e6391456c67d96a1
Amit Kumar10 <amit.kumar10@olacabs.com>
2022-07-21 17:29:20 +0530

    use only html code with v-html

--- src/components/htmlbinding.vue
+++ src/components/htmlbinding.vue
@@ -2,15 +2,25 @@
 <div>
 
 
-    <h4>this is come from html binding file</h4>
+    <h4>{{text}}</h4>
+    <h2>{{tags}}</h2> <!--by this way interpolate the all text with the tag this its print like this-><h2>this is come from tag</h2>"-->
+    <h1 v-html="tags"></h1>  <!--by this way we can use only html code without tag  it print this is come from tag-->
 </div>
 
 
 </template>
 
 <script>
+
+
 export default{
-    name:"html-biding"
+    name:"html-biding",
+    data(){
+        return{
+            text:"this is come form text",
+            tags:"<h2>this is come from tag</h2>"
+        }
+    }
 }
 
 
