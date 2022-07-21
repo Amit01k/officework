c59b22f04dab42608b3c74f4c05265a6e1199165
Amit Kumar10 <amit.kumar10@olacabs.com>
2022-07-21 17:15:27 +0530

    use strict

--- arrow_function/arrow.js
+++ arrow_function/arrow.js
@@ -13,5 +13,27 @@
 
 
 
-nums=[1,2,3,5,6,7]
-console.log(...nums)
+// nums=[1,2,3,5,6,7]
+// console.log(...nums)
+
+// b=nums.push(9)
+// console.log(b)
+
+"use strict"
+ a=10    //"use strict"; Defines that JavaScript code should be executed in "strict mode".
+console.log(a)
+function add(){
+    
+     b=10
+    console.log(b)
+
+}
+add()
+
+function add(){
+    "use strict"
+    b=10
+   console.log(b)
+
+}
+add()
