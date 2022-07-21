6c51c48bce1cd12f28250cb80d3466bb4b041adf
Amit Kumar10 <amit.kumar10@olacabs.com>
2022-07-21 17:50:21 +0530

    buttons tags using v-html

--- src/components/htmlbinding.vue
+++ src/components/htmlbinding.vue
@@ -5,9 +5,12 @@
     <h4>{{text}}</h4>
     <h2>{{tags}}</h2> <!--by this way interpolate the all text with the tag this its print like this-><h2>this is come from tag</h2>"-->
     <h1 v-html="tags"></h1>  <!--by this way we can use only html code without tag  it print this is come from tag-->
+
+    <button v-on:click="getName()"><h2 v-html="button"></h2> </button> <!--something new on line nu 9 -->
+
+
+
 </div>
-
-
 </template>
 
 <script>
@@ -18,7 +21,13 @@ export default{
     data(){
         return{
             text:"this is come form text",
-            tags:"<h2>this is come from tag</h2>"
+            tags:"<h2>this is come from tag</h2>",
+            button:"<button >click me</button>"
+        }
+    },
+    methods:{
+        getName(){
+            alert("this is come from alert with button")
         }
     }
 }
