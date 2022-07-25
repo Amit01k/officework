a9f61d833995cc8b0511bde1ed43a705178cab98
Amit Kumar10 <amit.kumar10@olacabs.com>
2022-07-25 12:14:44 +0530

    js questions

--- question/addtion_of_numbers.js
+++ question/addtion_of_numbers.js
@@ -1,7 +1,16 @@
-arr=[1,2,3,5]
-s=0
-for(let i=0;i<arr.length;i++){
-    s=s+arr[i]
+// arr=[1,2,3,5]
+// s=0
+// for(let i=0;i<arr.length;i++){
+//     s=s+arr[i]
 
-}
-console.log(s)
+// }
+// console.log(s)
+
+
+// const length = 4;
+// const numbers = [];
+// for (var i = 0; i < length; i++);{
+//   numbers.push(i +1);
+// }
+
+// console.log(numbers)
