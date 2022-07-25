47a14e922d6947ea19f1d102c5b5e9c72ce8e732
Amit Kumar10 <amit.kumar10@olacabs.com>
2022-07-25 12:55:14 +0530

    js questions

--- question/addtion_of_numbers.js
+++ question/addtion_of_numbers.js
@@ -15,9 +15,29 @@
 
 // console.log(numbers)
 
-a1 = 10;
-var a1;
-console.log(a1);
-////////////////////
-console.log(b1);
-var b1=20;
+// a1 = 10;
+// var a1;
+// console.log(a1);
+// ////////////////////
+// console.log(b1);
+// var b1=20;
+
+
+// const name = 'Lydia Hallie';
+// const age = 21;
+
+// console.log(Number.isNaN(name));
+// console.log(Number.isNaN(age));
+
+// console.log(isNaN(name));
+// console.log(isNaN(age));
+
+
+
+const groceries = ['banana', 'apple', 'peanuts'];
+
+if (groceries.indexOf('banana')) {     //herewe give indexOf banana so that is return 0 so this is false value than print else condition
+  console.log('We have to buy bananas!');
+} else {
+  console.log(`We don't have to buy bananas!`);
+}
