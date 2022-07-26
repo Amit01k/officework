5da23cb1a1f2d163c8ffee06151521617e2af6a7
Amit Kumar10 <amit.kumar10@olacabs.com>
2022-07-26 12:58:32 +0530

    tailwind css added

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,7 @@
 <template>
-    <div>
-        <nuxt-link to="/about">go to another page</nuxt-link>
-        <h1>This is home page</h1>
+    <div class="m-4">
+        <nuxt-link to="/about" class="decoration-sky-600">go to another page</nuxt-link>
+        <h1 class="text-blue-800">This is home page</h1>
+        <button type="button" class="bg-gray-900 text-base text-green-500" to="/">click me</button>
     </div>
 </template>
-<style>
-
-h1{
-    color: red;
-    background-color: green;
-}
-
-</style>
