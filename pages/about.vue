a8e126ed8184425448f12d47b32d174345b428e5
Amit Kumar10 <amit.kumar10@olacabs.com>
2022-07-26 12:19:21 +0530

    nuxt 3

---
+++ pages/about.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <nuxt-link to="/">home</nuxt-link>
+    <div>djgbjbfdjgbjdfgbj</div>
+    </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <nuxt-link to="/about">go to another page</nuxt-link>
+        <h1>This is home page</h1>
+    </div>
+</template>
+<style>
+
+h1{
+    color: red;
+    background-color: green;
+}
+
+</style>
