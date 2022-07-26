8b8c056e8f0f46105291f1044a158a40e06af01d
Amit Kumar10 <amit.kumar10@olacabs.com>
2022-07-26 17:00:34 +0530

    dynamic page

--- pages/index.vue
+++ pages/index.vue
@@ -3,16 +3,19 @@
         <nuxt-link to="/about" class="decoration-sky-600">go to another page</nuxt-link>
         <h1 class="text-blue-800">This is home page</h1>
         <button type="button" class="bg-gray-900 text-base text-green-500" to="/">click me</button>
-        <compo/>
+        <!-- <compo/> -->
+        <!-- <h2>hiii{{state.firstName}}</h2> -->
     </div>
 </template>
-<script>   //this is not working properly
-import compo from "../components/component.vue"
-definePageMeta({
-    components:{
-        compo
 
-    }
-})
 
-</script>
+<!-- <script >
+
+
+const state=reactive({
+    firstName:"amit",
+    lastName:"Kumae"
+});
+
+
+</script> -->
---
+++ pages/posts/[id].vue
@@ -0,0 +1,7 @@
+<template>
+
+    <div>
+        <nuxt-link to="/">home</nuxt-link>
+        <h2 class="text-center">this is a dynamic page {{$route.params.id}}</h2>
+    </div>
+</template>
