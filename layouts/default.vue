71132c3309b665a2e7b0cf211f8abfe177af1731
Amit Kumar10 <amit.kumar10@olacabs.com>
2022-07-27 11:40:22 +0530

    changes in dafault layout(added nuxt link)

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,9 +2,20 @@
   <div>
     <!-- <nuxt-link to="/about">go to about page</nuxt-link>
     <nuxt-link to="/">home</nuxt-link> -->
-    <h2 class="text-red-800"><nav>this is come from layouts </nav></h2>
-    <slot />
-    
+    <!-- <h2 class="text-red-800"><nav>this is come from layouts </nav></h2>
+    <slot /> -->
+    <!-- <ul>
+        <li><a href=""></a></li>
+        <li><a href=""></a></li>      ul>li*3>a
+        <li><a href=""></a></li>
+    </ul>
+     -->
+     <ul>
+        <!-- <li><nuxt-link to="/">home</nuxt-link></li> -->
+        <li><nuxt-link to="/about">about page</nuxt-link></li>
+        <li><nuxt-link to="/posts">post</nuxt-link></li>
+     </ul>
+     <slot/>
 
   </div>
 </template>
