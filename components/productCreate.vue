fffd5c7f4cc9a966d8e6f9806e3ba8dd7cbb0b1e
Amit Kumar10 <amit.kumar10@olacabs.com>
2022-07-28 15:40:09 +0530

    product page working

--- components/productCreate.vue
+++ components/productCreate.vue
@@ -1,6 +1,6 @@
-<!-- <template>
+<template>
 
-<h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae at repellat aut nulla, repellendus iure dolore vero numquam ipsum iusto cupiditate, architecto repudiandae fugiat quia nisi nostrum ab doloremque? Incidunt!</h3>
+<!-- <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae at repellat aut nulla, repellendus iure dolore vero numquam ipsum iusto cupiditate, architecto repudiandae fugiat quia nisi nostrum ab doloremque? Incidunt!</h3> -->
 
 
-</template> -->
+</template>
--- pages/createPage.vue
+++ pages/createPage.vue
@@ -1,14 +1,92 @@
 <template>
 
 <div>
-    <form>
+    <form @submit.prevent="submit">
         <label>title</label>
-        <input type="text"/>
+        <input type="text" v-model="title"/>
+
         <label>image</label>
-        <input type="text"/>
+        <input type="text"  v-model="image"/>
 
         <button>Add Button </button>
     </form>
+    <nuxt-link to="product">go to product page</nuxt-link>
 </div>
 
 </template>
+
+
+<script>
+import {ref} from 'vue'
+
+import { useRouter } from 'vue-router'
+//import { routerKey } from 'vue-router'
+
+export default{
+    name:"createPage",
+    setup(){
+        const title=ref("")
+        const image=ref("")
+        //console.log(title,image)
+         const router=useRouter()
+        const submit=async()=>{
+            await fetch("http://localhost:3000/products",{
+                method:"POST",
+                headers:{"content-type":"application/json"},
+                body:JSON.stringify({
+                    title:title.value,
+                    image:image.value
+                })
+            })
+            
+            //  await router.push("pages/product")
+            
+        }
+        return {title,image,submit}
+    }
+}
+
+
+
+</script>
+
+
+<!-- 
+<template>
+  <form @submit.prevent="submit" >
+    <label>Title</label>
+    <input type="text" name="title" v-model="title"                
+    />
+    <label>Image</label>
+    <input type="text" name="image" v-model="image" 
+    />
+    <button>Add Product</button>
+</form>
+</template>
+
+<script>
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+export default {
+    // name: "ProductCreate",
+    setup() {
+       const title = ref(''); 
+       const image = ref('');
+       const router = useRouter();
+       const submit = async () => {
+           await fetch('http://localhost:3000/products', {
+               method: 'POST',
+               headers: {"Content-type": "application/json"},
+               body: JSON.stringify({ 
+                   title: title.value,
+                   image: image.value
+                })
+           });
+           await router.push('data/products');
+       }
+       
+       return {title, image, submit}
+    }
+    
+}
+</script> -->
