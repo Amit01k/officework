fa93a59f8b76076431d00a9d7b7d0ea17a4f0f08
Amit Kumar10 <amit.kumar10@olacabs.com>
2022-07-29 12:40:14 +0530

    update button added

--- pages/ProductEdit.vue
+++ pages/ProductEdit.vue
@@ -1,7 +1,63 @@
 <template>
 
+<div>
+    <form @submit="submit">           <!--you can use prevent  to stop refreshing-->
+        <label class="text-slate-100 bg-red-900" >title</label>
+        <input type="text" v-model="title"/>
 
+        <label>image</label>
+        <input type="text"  v-model="image"/>
 
-
+        <button>Save </button>
+    </form>
+    <nuxt-link to="product">go to product page</nuxt-link>
+</div>
 
 </template>
+
+
+<script>
+import {ref,onMounted} from 'vue'
+
+import { useRouter,useRoute } from 'vue-router'
+//import { routerKey } from 'vue-router'
+
+export default{
+    name:"productEdit",
+    setup(){
+        const title=ref("")
+        const image=ref("")
+        //console.log(title,image)
+         const router=useRouter()
+         const route=useRoute()
+
+        onMounted(async()=>{
+            const res=await fetch(`http://localhost:3000/products/${route.params.id}`)
+            
+            const product=await res.json();
+            title.value=product.title;
+            image.value=product.image
+        
+        
+        })
+
+        const submit=async()=>{
+            await fetch("http://localhost:3000/products",{
+                method:"POST",
+                headers:{"content-type":"application/json"},
+                body:JSON.stringify({
+                    title:title.value,
+                    image:image.value
+                })
+            })
+            
+            //  await router.push("pages/product")
+            
+        }
+        return {title,image,submit}
+    }
+}
+
+
+
+</script>
